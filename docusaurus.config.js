814d0a2675ee48d74ae0b3ba25250110605b8b59
RJ <rjx21@proton.me>
2025-10-03 12:25:21 -0500

    fix deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Actuator Docs',
   favicon: 'img/favicon.ico',
+  githubHost: 'github-actuator', 
 
   // Set the production url of your site here
   // url: 'https://docs.actuator.finance',
